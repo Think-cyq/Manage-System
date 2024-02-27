082673f9917e402112b32e1cc622edeefc4942fa
think-cyq <2380494120@qq.com>
2024-02-27 21:36:04 +0800

    更改

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,11 +5,12 @@
         <span :class="collapseBtnClass" style="cursor: pointer;font-size: 20px" @click="SonCollapse"></span>
 
         <el-breadcrumb separator="/" style="display: inline-block;margin-right: 10px">
-          <el-breadcrumb-item :to="{ path: '/' }">Home</el-breadcrumb-item>
+          <el-breadcrumb-item :to="'/'">First-Page</el-breadcrumb-item>
+          <el-breadcrumb-item>{{ currentPathName }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
       <el-dropdown style="width: 70px; cursor: pointer">
-        <span>王小虎</span><i class="el-icon-arrow-down" style="margin-left: 5px"></i>
+        <span>王小虎</span><i class="el-icon-arrow-down" style=" margin-left: 5px"></i>
         <el-dropdown-menu slot="dropdown">
           <el-dropdown-item>information</el-dropdown-item>
           <el-dropdown-item>sign out</el-dropdown-item>
--- src/views/User.vue
+++ src/views/User.vue
@@ -110,7 +110,6 @@ export default {
       form:{},
       dialogFormVisible: false,
       multipleSelection: [],
-      headerBg: 'headerBg'
     }
   },
   created() {
